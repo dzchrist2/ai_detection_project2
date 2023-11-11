05d4fecdd950f435d27dbd94d2603e1117c78543
dzchrist2 <dzchrist@asu.edu>
2023-11-10 21:23:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import pandas as pd
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+
 
 filepath = 'data/train.csv'
 
 train = pd.read_csv(filepath, names=['id', 'title', 'abstract', 'introduction', 'label'])
+# print(train)
 
-print(train)
+# create a count vectorizer object 
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(train['title'])
+# print(count_vect.vocabulary_)
+# print("\n\n")
+count_vect.fit(train['abstract'])
+# print(count_vect.vocabulary_)
+count_vect.fit(train['introduction'])
 
+# Encoding
+encoder = LabelEncoder()
 
+encodedLabels = encoder.fit_transform(train['label'])
 
