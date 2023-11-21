895c786ded344c604abe4084e1f6f4672fc647d6
dzchrist2 <dzchrist@asu.edu>
2023-11-21 00:26:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,88 @@
 import pandas as pd
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import MultinomialNB
 
 
 
-filepath = 'data/train.csv'
+# Load Datasets to Data Frames
+trainFilepath = 'data/train.csv'
+testFilepath = 'data/test.csv'
+trainData = pd.read_csv(trainFilepath, names=['id', 'title', 'abstract', 'introduction', 'label'])
+testData = pd.read_csv(testFilepath, names=['id', 'title', 'abstract', 'introduction', 'label'])
 
-train = pd.read_csv(filepath, names=['id', 'title', 'abstract', 'introduction', 'label'])
-# print(train)
 
-# create a count vectorizer object 
-count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
-count_vect.fit(train['title'])
-# print(count_vect.vocabulary_)
-# print("\n\n")
-count_vect.fit(train['abstract'])
-# print(count_vect.vocabulary_)
-count_vect.fit(train['introduction'])
+# Split into Train and Validate Datasets
+train, valid = train_test_split(trainData)
 
-# Encoding
+# Label Encoder
 encoder = LabelEncoder()
-encodedLabels = encoder.fit_transform(train['label'])
+train_encodedLabels = encoder.fit_transform(train['label'])
+valid_encodedLabels = encoder.fit_transform(valid['label'])
 
+
+
+# Count Vectors:
+# Train: Create and Transform Count vectorizer object 
+trainCount_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+trainCount_vect.fit(train['abstract'])
+# print(trainCount_vect.vocabulary_)
+# print("\n\n")
+trainCount_vect.fit(train['title'])
+# print(trainCount_vect.vocabulary_)
+trainCount_vect.fit_transform(train['introduction'])
+valid_count = trainCount_vect.transform(valid)
+
+
+# Test: Create and Transform Count vectorizer object 
+testCount_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+testCount_vect.fit(train['abstract'])
+# print(testCount_vect.vocabulary_)
+# print("\n\n")
+testCount_vect.fit(train['title'])
+# print(testCount_vect.vocabulary_)
+testCount_vect.fit_transform(train['introduction'])
+
+
+
+# TF-IDF:
+# Word Level TF-IDFs
+title_tfidf = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+abstract_tfidf = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+intro_tfidf = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+title_tfidf.fit_transform(train['title'])
+title_valid_tfidf = title_tfidf.transform(valid['title'])
+abstract_tfidf.fit_transform(train['abstract'])
+abstract_valid_tfidf = abstract_tfidf.transform(valid['abstract'])
+intro_tfidf.fit_transform(train['introduction'])
+intro_valid_tfidf = intro_tfidf.transform(valid['introduction'])
+
+
+# TODO: add ngram level and character levels?
+
+
+# Function: Train Model
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+    # fit the training dataset on the classifier
+    classifier.fit(feature_vector_train, label)
+    
+    # predict the labels on validation dataset
+    predictions = classifier.predict(feature_vector_valid)
+    
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    return accuracy_score(predictions, valid_encodedLabels)
+
+
+# Naive Bayes Classification:
+# NB Count Vector
+accuracy = train_model(MultinomialNB(), trainCount_vect, train_encodedLabels, valid_count)
+print("NB, Count Vectors: ", accuracy)
 
 
